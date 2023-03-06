57e962bc25b7fd0b1abfebd64635a9f855fa6d33
Macca5357 <19237766@studentmail.ul.ie>
2023-03-06 15:38:17 +0000

    Stage 2

--- app/src/main/java/com/diary/app/MainActivity.java
+++ app/src/main/java/com/diary/app/MainActivity.java
@@ -6,8 +6,15 @@ package com.diary.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextInputText
+            ,editTextInputDate;
+    Button buttonSubmit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
